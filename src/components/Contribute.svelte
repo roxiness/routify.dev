c2e68ae797fa32fab6395c3cbebff8b2604d99f7
evajacobs <ej@mono.company>
2020-02-24 17:18:45 +0100

    Spacing and responsiveness adjustments

--- src/components/Contribute.svelte
+++ src/components/Contribute.svelte
@@ -2,11 +2,14 @@
     div {
         text-align: center;
     }
+
 </style>
 
 <div>
-    <p>Want to help? Use Routify, file issues, pick up issues. Let’s make the best router for Svelte 3!</p>
-    <p>Give us a star on Github:</p>
+    <p>Want to help? Use Routify, file issues, pick up issues.</p>
+    <p>Let’s make the best router for Svelte 3!</p>
+    <br/>
+    <p class="u-spacer-bottom--small">Give us a star on Github:</p>
     <!-- Place this tag where you want the button to render. -->
     <a class="github-button" href="https://github.com/sveltech/routify" data-icon="octicon-star" data-show-count="true" aria-label="Star sveltech/routify on GitHub">Star</a>
 </div>
