c73615169b3f9a5b1b1e1aec545c3dbd96892608
evajacobs <ej@mono.company>
2020-02-24 12:38:53 +0100

    Correct font sizes and spacing - Hero

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,10 +1,8 @@
 <div class="c-hero">
     <div class="c-hero__content">
-        <h2 class="c-h1">Routes for Svelte, automated by your file structure.</h2>
-        <div class="c-content">
-            <p>Routify offers a great developer experience to build the routes for your Svelte app.</p>
-        </div>
-        <p><a href="/installation" class="c-button c-button--primary c-button--large">Get started</a></p>
+        <h2 class="c-h1">Routes for Svelte, automated <br/> by your file structure.</h2>
+        <p>Routify offers a great developer experience to build the routes <br/> for your Svelte app.</p>
+        <a href="/installation" class="c-button c-button--primary c-button--large">Get started</a>
     </div>
     <div class="c-hero__illustration">
         <img src="/images/illustration.png">
