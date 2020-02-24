2a60fcf1e880adc63d1118987ce63db16cd59d07
evajacobs <ej@mono.company>
2020-02-24 13:41:48 +0100

    Make hero responsive

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,10 +1,10 @@
 <div class="c-hero">
     <div class="c-hero__content">
-        <h2 class="c-h1">Routes for Svelte, automated <br/> by your file structure.</h2>
-        <p>Routify offers a great developer experience to build the routes <br/> for your Svelte app.</p>
+        <h2 class="c-h1">Routes for Svelte, automated by your file structure.</h2>
+        <p>Routify offers a great developer experience to build the routes for your Svelte app.</p>
         <a href="/installation" class="c-button c-button--primary c-button--large">Get started</a>
     </div>
     <div class="c-hero__illustration">
-        <img src="/images/illustration.png">
+        <img src="/images/illustration.png" alt="Image of file structure">
     </div>
 </div>
