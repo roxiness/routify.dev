9f33c40c2cb93a905cb9eff1e24401bd110daf42
evajacobs <ej@mono.company>
2020-02-24 13:54:02 +0100

    Styling contributors

--- src/components/Contributors.svelte
+++ src/components/Contributors.svelte
@@ -18,50 +18,20 @@
     ]
 </script>
 
-<style>
+<div class="c-contributors">
 
-    h3 {
-        text-align: center;
-    }
+    <h3 class="c-h3">Contributors</h3>
 
-    ul {
-        display: flex;
-        text-align: center;
-        justify-content: center;
-    }
+    <ul class="c-contributors__list">
+        {#each contributors as contributor}
+        <li class="c-contributors__list-item">
+            <a href="{contributor.github_url}">
+                <img class="c-contributors__img" src="/images/{contributor.imgpath}.jpg" alt="contributor image">
+                <span class="c-contributors__name">{contributor.name}</span>
+            </a>
+        </li>
+        {/each}
+    </ul>
+</div>
 
-    li {
-        flex: 0 0 auto;
-        padding: 0 3.2rem;
-    }
-
-    a {
-        display: block;
-        color: #000;
-        text-decoration: none;
-    }
-
-    img {
-        border-radius: 100%;
-        width: 4.8rem;
-        height: 4.8rem;
-    }
-
-    span {
-        display: block;
-    }
-</style>
-
-<h3 class="c-h3">Contributors</h3>
-
-<ul>
-    {#each contributors as contributor}
-    <li>
-        <a href="{contributor.github_url}">
-            <img src="/images/{contributor.imgpath}.jpg">
-            <span>{contributor.name}</span>
-        </a>
-    </li>
-    {/each}
-</ul>
 
