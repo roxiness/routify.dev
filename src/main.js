de33456efea0e496e26b5dfbb1c2f7f1444c0ec2
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-02-26 17:49:53 +0100

    scss

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import HMR from '@sveltech/routify/hmr'
 import App from './App.svelte';
-import '../scss/style.scss'
-
 const app = HMR(App, { target: document.body }, 'routify-app')
 
 export default app;
