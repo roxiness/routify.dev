6118256c515be0ee71a9925ed25ecbeaa7ac889f
Johan Ronsse <johan.ronsse@gmail.com>
2020-02-27 23:18:14 +0100

    Improve hero on mobile

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,7 +1,7 @@
 <div class="c-hero">
     <div class="c-hero__content">
-        <h2 class="c-h1">Routes for Svelte, automated by your file structure.</h2>
-        <p>Routify offers a great developer experience to build the routes for your Svelte app.</p>
+        <h2 class="c-hero__title">Routes for Svelte, automated by your file structure.</h2>
+        <p class="c-hero__para">Routify offers a great developer experience to build the routes for your Svelte app.</p>
         <a href="/getting-started" class="c-button c-button--primary c-button--large">Get started</a>
     </div>
     <div class="c-hero__illustration">
