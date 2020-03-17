84a7da3126549bf94e69e51b41d053a7f71b477b
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-03-17 15:12:14 +0100

    cleanup auth

--- src/pages/examples/auth.svelte
+++ src/pages/examples/auth.svelte
@@ -18,21 +18,24 @@
     Since everything in Routify is scoped, protecting a layout, automatically
     protects all nested components recursively.
   </p>
+</div>
 
-  <h2>Authentication guards</h2>
-  <p>
-    Users should generally be authenticated in the root layout.
-    <Tabs>
-      <div class="c-tabs">
-        <TabsLink>Minimal</TabsLink>
-        <TabsLink>Basic</TabsLink>
-        <TabsLink>Store</TabsLink>
+<div class="c-container-vertical--small">
+  <div class="c-content">
+    <h2>Authentication guards</h2>
+    <p>
+      Users should generally be authenticated in the root layout.
+      <Tabs>
+        <div class="c-tabs">
+          <TabsLink>Minimal</TabsLink>
+          <TabsLink>Basic</TabsLink>
+          <TabsLink>Store</TabsLink>
 
-      </div>
-      <div class="c-tabs-pages">
-        <TabsPage>
-          <Prism language="svelte">
-            {`<!-- src/pages/_layout.svelte -->
+        </div>
+        <div class="c-tabs-pages">
+          <TabsPage>
+            <Prism language="svelte">
+              {`<!-- src/pages/_layout.svelte -->
 <scr`}{`ipt>
   import { authenticate } from 'my-auth-service'
 </script>
@@ -42,79 +45,80 @@ This prevents small UI glitches and premature authorization checks. -->
 {#await authenticate()}
 {:then}
   <slot />
-{/await}
-`}
-          </Prism>
-        </TabsPage>
-        <TabsPage>
-          <Prism language="svelte">
-            {`<!-- src/pages/_layout.svelte -->
+{/await}`}
+            </Prism>
+          </TabsPage>
+          <TabsPage>
+            <Prism language="svelte">
+              {`<!-- src/pages/_layout.svelte -->
 <scr`}{`ipt>
   import { authenticate } from 'my-auth-service'
 </script>
 
-<!-- Wait with rendering the app till the user/guest is verified.
-This prevents small UI glitches and premature authorization checks. -->
+  <!-- Wait with rendering the app till the user/guest is verified.
+  This prevents small UI glitches and premature authorization checks. -->
 {#await authenticate()}
 
   <!-- Show a waiting message/page/animation here -->
   Verifying user...
 
-<!-- Pass the user to all nested pages in the project -->
+  <!-- Pass the user to all nested pages in the project -->
 {:then user}
   <slot scoped={{user}} />
-{/await}
-`}
-          </Prism>
-        </TabsPage>
-        <TabsPage>
-          <Prism language="svelte">
-            {`<!-- src/pages/_layout.svelte -->
+{/await}`}
+            </Prism>
+          </TabsPage>
+          <TabsPage>
+            <Prism language="svelte">
+              {`<!-- src/pages/_layout.svelte -->
 <scr`}{`ipt>
   import { user } from '../store'
 </script>
 
 {#if user}
   <slot />
-{/if}
+{/if}`}
+            </Prism>
+          </TabsPage>
+        </div>
+      </Tabs>
 
-`}
-          </Prism>
-        </TabsPage>
+    </p>
+  </div>
+</div>
+
+<div class="c-container-vertical--small">
+  <div class="c-content">
+    <h2>Authorization Guards</h2>
+    <p>
+      Guards should be implemented in the component or module which they
+      protect.
+    </p>
+
+    <Tabs>
+      <div class="c-tabs">
+        <TabsLink>Scoped</TabsLink>
+        <TabsLink>Async (redirect)</TabsLink>
+        <TabsLink>Global guard</TabsLink>
+        <TabsLink>Global guard (redirect)</TabsLink>
+
       </div>
-    </Tabs>
-
-  </p>
-  <h2>Authorization Guards</h2>
-  <p>
-    Guards should be implemented in the component or module which they protect.
-  </p>
-
-  <Tabs>
-    <div class="c-tabs">
-      <TabsLink>Scoped</TabsLink>
-      <TabsLink>Async (redirect)</TabsLink>
-      <TabsLink>Global guard</TabsLink>
-      <TabsLink>Global guard (redirect)</TabsLink>
-
-    </div>
-    <div class="c-tabs-pages">
-      <TabsPage>
-        <Prism language="svelte">
-          {`<!-- src/pages/admin/_layout.svelte -->
+      <div class="c-tabs-pages">
+        <TabsPage>
+          <Prism language="svelte">
+            {`<!-- src/pages/admin/_layout.svelte -->
 <scr`}{`ipt>
   export let scoped
 </script>
 
 {#if scoped.user.isAdmin}
   <slot />
-{/if}
-`}
-        </Prism>
-      </TabsPage>
-      <TabsPage>
-        <Prism language="svelte">
-          {`<!-- src/pages/admin/_layout.svelte -->
+{/if}`}
+          </Prism>
+        </TabsPage>
+        <TabsPage>
+          <Prism language="svelte">
+            {`<!-- src/pages/admin/_layout.svelte -->
 <scr`}{`ipt>
   import { goto, routifyContext } from '@sveltech/routify'
   import { authorize } from 'my-auth-service'
@@ -126,15 +130,14 @@ This prevents small UI glitches and premature authorization checks. -->
   <slot />
 {:catch}
   {$goto('/login')}
-{/await}
-`}
-        </Prism>
+{/await} `}
+          </Prism>
 
-      </TabsPage>
-      <!-- Global guard -->
-      <TabsPage>
-        <Prism language="svelte">
-          {`<!-- src/pages/_layout.svelte -->
+        </TabsPage>
+        <!-- Global guard -->
+        <TabsPage>
+          <Prism language="svelte">
+            {`<!-- src/pages/_layout.svelte -->
 <scr`}{`ipt>
   import { goto } from '@sveltech/routify'
   import { authenticate } from 'my-auth-service'
@@ -152,20 +155,18 @@ This prevents small UI glitches and premature authorization checks. -->
 
 {#if user}
   <slot scoped={{user}} />
-<!-- wait in case there's a session that we need to verify -->
+  <!-- wait in case there's a session that we need to verify -->
 {:else if sessionVerified}
   <!-- User is a guest. Show a login widget instead of the current page. -->
   <Login let:user={user}>
-{/if}
+{/if}`}
+          </Prism>
 
-`}
-        </Prism>
-
-      </TabsPage>
-      <!-- Global guard (redirect) -->
-      <TabsPage>
-        <Prism language="svelte">
-          {`<!-- src/pages/_layout.svelte -->
+        </TabsPage>
+        <!-- Global guard (redirect) -->
+        <TabsPage>
+          <Prism language="svelte">
+            {`<!-- src/pages/_layout.svelte -->
 <scr`}{`ipt>
   import { goto, routifyContext } from '@sveltech/routify'
   import { authorize } from 'my-auth-service'
@@ -176,26 +177,29 @@ This prevents small UI glitches and premature authorization checks. -->
   /** else only show if user is authorized. Otherwise, redirect to login. **/
   if(!show)
     authorize()
-      .then( ()=>{ show = true } )
-      .catch( ()=> $goto('/login') )
+    .then( ()=>{ show = true } )
+    .catch( ()=> $goto('/login') )
 </script>
 
 {#if show}
   <slot />
-{/if}
-`}
-        </Prism>
+{/if}`}
+          </Prism>
 
-      </TabsPage>
-    </div>
-  </Tabs>
+        </TabsPage>
+      </div>
+    </Tabs>
+  </div>
+</div>
+<div class="c-container-vertical--small">
+  <div class="c-content">
+    <h3>Realtime guards</h3>
+    <p>
+      For realtime guards, simply replace
+      <code>{'{#await <promise>}'}</code>
+      with
+      <code>{'{#if <reactive condition>}'}</code>
+    </p>
 
-  <h3>Realtime guards</h3>
-  <p>
-    For realtime guards, simply replace
-    <code>{'{#await <promise>}'}</code>
-    with
-    <code>{'{#if <reactive condition>}'}</code>
-  </p>
-
+  </div>
 </div>
