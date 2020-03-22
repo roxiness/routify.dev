73157d59e1dcab1b43a132495d2ca7fe5ee7a49d
Johan Ronsse <johan.ronsse@gmail.com>
2020-03-22 12:33:38 +0100

    Implement accordion for mobile

---
+++ src/components/NavItemMobile.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let href;
+
+    import { mobileNavActive } from '../stores/mobilenavactive.js';
+
+    function handleNavigationClick(e) {
+        mobileNavActive.update(mobileNavActive => false);
+    }
+
+</script>
+
+<!-- c-navigation-mobile__item--active -->
+<li class="c-navigation-mobile__item ">
+    <a on:click="{handleNavigationClick}" href="{href}">
+        <slot></slot>
+    </a>
+</li>
