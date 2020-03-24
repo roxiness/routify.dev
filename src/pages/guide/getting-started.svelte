d024a31f7e1f76c3286d419e19b878433389c0c1
Johan Ronsse <johan.ronsse@gmail.com>
2020-03-24 17:38:11 +0100

    Improve instructions for getting started, point to starter repo

--- src/pages/guide/getting-started.svelte
+++ src/pages/guide/getting-started.svelte
@@ -12,9 +12,11 @@
     <h1 class="c-h1">Getting started</h1>
     <div class="c-content">
       <p>
-        To create a new Routify project, open your console and type
+        To create a new Routify project, create a new folder, then navigate to it, open your terminal and type
         <code>npx @sveltech/routify init</code>
       </p>
+      <p>This will initialize the Routify starter project (see this <a href="https://github.com/sveltech/routify-starter">repo</a>) in the folder.</p> 
+      <p>Alternatively, download the repository and follow the instructions.</p> 
       <p>
           If you wish to add Routify to an existing project, please refer to our
           <a href={$url('../../guide/install')}>install guide</a>
