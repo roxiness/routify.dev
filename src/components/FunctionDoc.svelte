e45e0250e6e86d77122c2925521937e2257e2a01
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-03-24 18:19:34 +0100

    FunctionDoc draft

---
+++ src/components/FunctionDoc.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let name = "";
+  export let params = [];
+</script>
+
+<div class="function-doc">
+  <h3>{name}</h3>
+  <ul>
+    {#each params as param}
+      <li>
+        <span class="type">{param.type}</span>
+        <span class="param-description">{param.description}</span>
+      </li>
+    {/each}
+  </ul>
+  <div class="description">
+    <slot />
+  </div>
+  <div class="note">
+    <slot name="note" />
+  </div>
+</div>
