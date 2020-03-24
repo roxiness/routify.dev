87efa43bc7f95bb4817214d3317cf8d20b44aeda
Johan Ronsse <johan.ronsse@gmail.com>
2020-03-24 18:56:59 +0100

    Basic styling

--- src/components/FunctionDoc.svelte
+++ src/components/FunctionDoc.svelte
@@ -2,21 +2,33 @@
   export let name = "";
   export let params = [];
 </script>
+  
+<style>
+  .c-function-doc {
+  }
 
-<div class="function-doc">
-  <h3>{name}</h3>
-  <ul>
-    {#each params as param}
-      <li>
-        <span class="type">{param.type}</span>
-        <span class="param-description">{param.description}</span>
-      </li>
-    {/each}
-  </ul>
-  <div class="description">
+  .c-function-doc__note {
+    background: #FBE4FD;
+    padding: 2.4rem;
+  }
+</style>
+
+<div class="c-function-doc">
+  <h3 class="c-h3">{name}</h3>
+  <div class="c-content">
+    <ul>
+      {#each params as param}
+        <li>
+          <span class="type">{param.type}</span>
+          <span class="param-description">{param.description}</span>
+        </li>
+      {/each}
+    </ul>
+  </div>
+  <div class="c-content">
     <slot />
   </div>
-  <div class="note">
+  <div class="c-function-doc__note">
     <slot name="note" />
   </div>
 </div>
