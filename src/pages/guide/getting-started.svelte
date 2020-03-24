89ebce99f9c77073b7b3ad1059831f73e51bebd4
Johan Ronsse <johan.ronsse@gmail.com>
2020-03-24 17:30:27 +0100

    Improve getting started template

--- src/pages/guide/getting-started.svelte
+++ src/pages/guide/getting-started.svelte
@@ -10,16 +10,15 @@
 <div class="c-container-horizontal--narrow">
   <div class="c-container-vertical--small">
     <h1 class="c-h1">Getting started</h1>
-    <p>
-      To create a new Routify project, open your console and type
-      <br />
-      <code>npx @sveltech/routify init</code>
-    </p>
-    <br />
-    <!-- TODO proper class -->
-    <p style="font-size: 90%">
-        If you wish to add Routify to an existing project, please refer to our
-        <a href={$url('../../guide/install')}>install guide</a>
-    </p>
+    <div class="c-content">
+      <p>
+        To create a new Routify project, open your console and type
+        <code>npx @sveltech/routify init</code>
+      </p>
+      <p>
+          If you wish to add Routify to an existing project, please refer to our
+          <a href={$url('../../guide/install')}>install guide</a>
+      </p>
+    </div>
   </div>
 </div>
