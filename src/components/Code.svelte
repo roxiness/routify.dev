7ed3d88e768268b94b7be147688c2e2244ac3db9
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-03-25 16:21:39 +0100

    added code component

---
+++ src/components/Code.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Prism from "svelte-prism";
+  export let language;
+  export let title;
+</script>
+
+<div>
+  {#if title}
+    <h5>{title}</h5>
+  {/if}
+  <Prism {language}>
+    <slot />
+  </Prism>
+  <slot name="caption" />
+</div>
