360494d8f28fb14224670585ed8a28e68e96a105
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-03-26 12:45:24 +0100

    add decorator example

--- src/pages/docs/decorators.svelte
+++ src/pages/docs/decorators.svelte
@@ -1,11 +1,11 @@
 <script>
   import Prism from "svelte-prism";
   import { meta } from "@sveltech/routify";
+  import Note from "@/components/Note.svelte";
   meta.title = "Decorators";
 </script>
 
 <!-- routify:options $index=30 -->
-
 
 <h1 class="c-h1">Decorators</h1>
 <div class="c-content">
@@ -16,5 +16,34 @@
   <p>Any component with a default slot can be a decorator.</p>
   <h3>Usage</h3>
   <Prism language="html">{`<slot decorator={MyDecorator} />`}</Prism>
-  <p>Decorators can be changed/disabled recursively on any layout or page.</p>
+
+  <h3>Example</h3>
+
+  <Prism language="html">
+    {`
+    <!-- _FadeDecorator.svelte -->
+    <`}{`script>
+      import { fade } from "svelte/transition";
+    </script>
+
+    <div transition:fade>
+      <slot />
+    </div>
+  `}
+  </Prism>
+
+  <Prism language="html">
+    {`
+      <!-- _layout.svelte -->
+      <`}{`script>
+        import FadeDecorator from "./_FadeDecorator.svelte";
+      </script>
+
+      <slot decorator={FadeDecorator} />
+  `}
+  </Prism>
+
+  <Note>
+    <p>Decorators can be changed/disabled recursively on any layout or page.</p>
+  </Note>
 </div>
