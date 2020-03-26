26156e5c8d9f1876b37ac18c75b9fdb48e6d5b89
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-03-26 17:33:14 +0100

    update link

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -2,7 +2,7 @@
     <div class="c-hero__content">
         <h2 class="c-hero__title">Routes for Svelte, automated by your file structure.</h2>
         <p class="c-hero__para">Routify offers a great developer experience to build the routes for your Svelte app.</p>
-        <a href="/getting-started" class="c-button c-button--primary c-button--large">Get started</a>
+        <a href="/introduction/getting-started" class="c-button c-button--primary c-button--large">Get started</a>
     </div>
     <div class="c-hero__illustration">
         <img src="/images/illustration.png" alt="Image of file structure">
