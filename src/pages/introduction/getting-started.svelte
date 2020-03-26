a7ad48ad304947266a3013de4da5a6fb6b565d15
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-03-26 17:33:22 +0100

    update

--- src/pages/introduction/getting-started.svelte
+++ src/pages/introduction/getting-started.svelte
@@ -2,21 +2,52 @@
   import { url, meta } from "@sveltech/routify";
   import Prism from "svelte-prism";
   import { Tabs, TabsLink, TabsPage } from "@sveltech/bricks";
-  meta.title = "Getting Started";
+  meta.title = "What is Routify";
+  import Note from "@/components/Note.svelte";
 </script>
 
 <!-- routify:options $index=10 -->
 
 <h1 class="c-h1">Getting started</h1>
 <div class="c-content">
+
   <p>
-    To create a new Routify project, create a new folder, then navigate to it, open your terminal and type
-    <code>npx @sveltech/routify init</code>
+    To create your first Routify app, simply run
+    <code>npx @sveltech/routfiy init</code>
+    in an empty folder.
   </p>
-  <p>This will initialize the Routify starter project (see this <a href="https://github.com/sveltech/routify-starter">repo</a>) in the folder.</p> 
-  <p>Alternatively, download the repository and follow the instructions.</p> 
   <p>
-      If you wish to add Routify to an existing project, please refer to our
-      <a href={$url('../../guide/install')}>install guide</a>.
+    This will install the Routify starter template, in which you'll find an
+    example folder (
+    <code>src/pages/examples</code>
+    ). You can experiment with these examples or just keep them as references.
+  </p>
+  <p>
+    When you're ready to get started on your app, simple delete the contents of
+    <code>src/pages/index.svelte</code>
+    and start writing your own content. You will still be able to access the
+    examples on
+    <code>http://localhost:5000/examples</code>
+    .
+  </p>
+
+  <p>
+    When you don't need the examples anymore you can simply delete the examples
+    folder.
+  </p>
+
+  <Note>
+    We recommend reading
+    <a href={$url('../structure')}>structure</a>
+    and
+    <a href={$url('../parameters')}>parameters</a>
+    before creating your first app.
+  </Note>
+
+  <p>
+    To get further aquainted with Routify we recommend out
+    <a href={$url('../../guide')}>Guide</a>
+    and
+    <a href={$url('../../docs')}>Docs</a>
   </p>
 </div>
