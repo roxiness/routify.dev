3df73ab0cf8a92272ae77780974db60f23cc8905
evajacobs <ej@mono.company>
2020-03-26 09:57:07 +0100

    Add note component

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -1,11 +1,14 @@
 <script>
     export let icon;
+    export let large = false;
 
     // Possible icon types
     import IconChevronUp from './icons/chevron-up.svg';
     import IconChevronDown from './icons/chevron-down.svg';
     import IconChevronLeft from './icons/chevron-left.svg';
     import IconChevronRight from './icons/chevron-right.svg';
+    import IconInfo from './icons/info.svg';
+    import IconWarning from './icons/warning.svg';
 
 </script>
 
@@ -15,9 +18,14 @@
         height: 1.6rem;
     }
 
+    .svg-icon--large {
+        width: 2.4rem;
+        height: 2.4rem;
+    }
+
 </style>
 
-<div class="svg-icon">
+<div class="svg-icon" class:svg-icon--large={large}>
 {#if icon == 'chevron-left'}
     {@html IconChevronLeft}
 {/if}
@@ -29,6 +37,12 @@
 {/if}
 {#if icon == 'chevron-up'}
     {@html IconChevronUp}
+{/if}
+{#if icon == 'info'}
+    {@html IconInfo}
+{/if}
+{#if icon == 'warning'}
+    {@html IconWarning}
 {/if}
 
 </div>
---
+++ src/components/Note.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let type;
+  import Icon from './Icon.svelte';
+</script>
+
+
+<div class="c-note c-note--{type}">
+  {#if type === "warning"}
+    <Icon large icon="warning" />
+  {:else}
+    <Icon large icon="info" />
+  {/if}
+  <p><slot></slot></p>
+</div>
