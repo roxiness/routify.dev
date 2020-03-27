4fb6c1905e71318491b713f5df8e59608dccbf8f
Johan Ronsse <johan.ronsse@gmail.com>
2020-03-27 14:48:15 +0100

    Add discord message + re-shuffle github star widget
    Move Github star link to hero: bigger chance for people to see this. Add discord message to the bottom of page: community section.

--- src/components/Contribute.svelte
+++ src/components/Contribute.svelte
@@ -4,10 +4,8 @@
     }
 </style>
 
-<div>
-    <p>Want to help? Use Routify, <a href="https://github.com/sveltech/routify/issues">file issues</a>, or pick up issues.</p>
+<div class="c-content">
+  <h3>Community</h3>
+    <p>Want to help? Need help? Use Routify, <a href="https://github.com/sveltech/routify/issues">check issues on Github</a>, or talk to us on <a href="https://discord.gg/5M3FE68"><img src="/images/discord.svg" width="24" style="position: relative; top: 0.5rem"> Discord</a> (this is the invite link).</p>
     <p>Let’s make the best router for Svelte 3!</p>
-    <br/>
-    <p class="u-spacer-bottom--small">Give us a star on Github:</p>
-    <a class="github-button" href="https://github.com/sveltech/routify" data-icon="octicon-star" data-show-count="true" aria-label="Star sveltech/routify on GitHub">Star</a>
 </div>
--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,10 +1,21 @@
+<style>
+  .specific-btn-toolbar a:first-child {
+    margin-right: 1.2rem;
+  }
+</style>
+
 <div class="c-hero">
     <div class="c-hero__content">
         <h2 class="c-hero__title">Routes for Svelte, automated by your file structure.</h2>
         <p class="c-hero__para">Routify offers a great developer experience to build the routes for your Svelte app.</p>
-        <a href="/introduction/getting-started" class="c-button c-button--primary c-button--large">Get started</a>
+        
+        <div class="specific-btn-toolbar">
+          <a href="/introduction/getting-started" class="c-button c-button--primary c-button--large">Get started</a>
+          <a class="github-button" href="https://github.com/sveltech/routify" data-icon="octicon-star" data-show-count="true" aria-label="Star sveltech/routify on GitHub">Star</a>
+        </div>
     </div>
     <div class="c-hero__illustration">
         <img src="/images/illustration.png" alt="Image of file structure">
     </div>
 </div>
+
