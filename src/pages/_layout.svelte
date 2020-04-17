c15ddabb1c8a6a150695db3fae84c891612b7510
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-04-17 21:53:51 +0200

    moved footer to bottom

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -7,14 +7,27 @@
   $: metatags.title = capitalize($page.title);
 </script>
 
+<style>
+  .c-container-horizontal {
+    display: flex;
+    flex-direction: column;
+    /* 4px for purple top bar */
+    min-height: calc(100vh - 0.4rem);
+  }
+  main {
+    flex: 1;
+  }
+</style>
+
 <!-- routify:options children=[
   {title: "Github", icon:"github", path: "https://github.com/sveltech/routify"},
   {title: "Discord", icon:"discord", path: "https://discord.gg/ntKJD5B"},
   {title: "Twitter", icon:"twitter", path: "https://twitter.com/routifyjs"}
 ] -->
-
 <div class="c-container-horizontal">
   <Header />
-  <slot />
+  <main>
+    <slot />
+  </main>
   <Footer />
 </div>
