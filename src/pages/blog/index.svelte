61dc2ac1198548796f75fe72cbc213bd67485012
Johan Ronsse <johan.ronsse@gmail.com>
2020-04-17 18:51:19 +0200

    Get rid of comments

--- src/pages/blog/index.svelte
+++ src/pages/blog/index.svelte
@@ -22,12 +22,10 @@
         <li class="c-blogpost-list__item">
           <article class="c-blogpost">
             <header>
-              <!-- @todo link to /blog/2020-04-06/a-start - so without the .md file name -->
               <h2>
                 <a href={$url('../:id', { id: post.id })}>{post.title}</a>
               </h2>
               <p>Written by {post.author.displayName}</p>
-              <!-- @todo substring to the first characters containing the date -->
             </header>
             {@html post.content}
           </article>
