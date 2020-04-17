c414b952a0d287bc41250b6a706ec668282b5d24
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-04-17 21:54:26 +0200

    added cache to fetcher

--- src/pages/blog/_fetcher.js
+++ src/pages/blog/_fetcher.js
@@ -1,12 +1,31 @@
+
 const api = 'AIzaSyDnR8LpwQN2RNCzPQ7qklVvmIhBks56uJs'
 const blogId = '5405362097133643550'
 const baseUrl = `https://www.googleapis.com/blogger/v3/blogs/${blogId}`
-const url = 'https://www.googleapis.com/blogger/v3/blogs/5405362097133643550/posts?key=AIzaSyDnR8LpwQN2RNCzPQ7qklVvmIhBks56uJs'
 
-/** @param {string} path the part after https://www.googleapis.com/blogger/v3/blogs/:blog-id/ */
-export async function fetcher(path) {
-  return fetch(`${baseUrl}${path}?key=${api}`, {
-    headers: { 'Content-Type': 'application/json' }
-  })
-    .then(res => res.json())
+const cache = {}
+
+/** prefetch blog index */
+fetcher('/posts')
+
+/**
+ * Sends data to callback
+ * Each call with a callback will rehydrate the cache
+ * Cached and rehydrated data will be sent to callback consecutively.
+ *
+ * @param {string} path the part after https://www.googleapis.com/blogger/v3/blogs/:blog-id/ */
+/** @param {function=} callback */
+export async function fetcher(path, callback) {
+  if (cache[path] && callback) callback(cache[path])
+
+  if (!cache[path] || callback) {
+    const res = await fetch(`${baseUrl}${path}?key=${api}`, {
+      headers: { 'Content-Type': 'application/json' }
+    })
+
+    cache[path] = await res.json()
+
+    if (callback) callback(cache[path])
+  }
 }
+
