52ca14ae75817308c1450a8b9b39fb3e1f131516
Johan Ronsse <johan.ronsse@gmail.com>
2020-04-18 14:16:10 +0200

    Delete leftover Stack component

--- src/components/Stack.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  export let space = null;
-  export let horizontal = null;
-</script>
-
-<div class="c-stack c-stack--{space} { horizontal ? "c-stack-horizontal--" + space : ''}">
-  <slot></slot>
-</div>
