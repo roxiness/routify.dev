73faeaf938448a3eed8b79f1453a2f8e2cb571d0
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-04-19 13:35:00 +0200

    query

---
+++ src/pages/docs/query-strings.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Code from "@/components/Code.svelte";
+</script>
+
+<!-- routify:options index=45 -->
+<div class="c-container-vertical--small">
+  <div class="c-content">
+    <h1 class="c-h1">Query strings</h1>
+
+    <p>
+     ToDO</p>
+
+
+  </div>
+</div>
