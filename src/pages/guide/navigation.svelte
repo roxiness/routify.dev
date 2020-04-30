6a93eaaf303c15b943f129842177f66887dd901d
WolfL <35525099+kl4rk@users.noreply.github.com>
2020-04-30 12:33:03 +0200

    Fix invalid HTML in the third example
    The third example was missing a ">" to close the list tag.
    Without the example wont compile.

--- src/pages/guide/navigation.svelte
+++ src/pages/guide/navigation.svelte
@@ -113,7 +113,7 @@
     <ul>
       {#each links as [path, name]}
         <!-- Svelte magic. If isActive is true, the "active" class is applied. -->
-        <li class:active={$isActive({path})}
+        <li class:active={$isActive({path})}>
           <a href={$url({path})}>
             {name}
           </a>
