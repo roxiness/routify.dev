6e3a53a8f5ab236a6b87a1880d1c7a11ceb00fdf
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-06-01 19:25:19 +0200

    add wip section and prefetch&cache draft

---
+++ src/pages/wip/index.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { page, url } from "@sveltech/routify";
+</script>
+
+<!-- routify:options index=false -->
+
+{#each $page.parent.children as child}
+  <div>
+    <a href={$url(child.path)}>{child.title}</a>
+  </div>
+{/each}
