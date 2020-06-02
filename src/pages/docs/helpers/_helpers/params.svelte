30e7911266f98202761461831ae28b1da412a9d9
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-06-02 10:26:45 +0200

    add documentation for queryHandler

--- src/pages/docs/helpers/_helpers/params.svelte
+++ src/pages/docs/helpers/_helpers/params.svelte
@@ -19,5 +19,23 @@
       </script>
   `}
   </Code>
+  <br />
+  <p>
+    By default, the query handler uses the native URLSearchParams function. For
+    more advanced usage, you can create your own
+    <code>queryHandler</code>
+    . The queryHandler is an object with a <code>parse</code> and a <code>stringify</code> method.
+  </p>
+  <Code language="svelte">
+    {`
+    <`}{`script>
+      ...
+      import { parse, stringify } from 'qs'
+      const queryHandler = { parse, stringify }
+    </script>
+
+    <Router config={{queryHandler}} ... />
+  `}
+  </Code>
 
 </FunctionDoc>
