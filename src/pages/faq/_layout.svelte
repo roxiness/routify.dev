8afe537ae536bc2db91cc664c1c22bfb662c38c0
Johan Ronsse <johan.ronsse@gmail.com>
2020-06-05 21:20:57 +0200

    Move FAQ to its own page

---
+++ src/pages/faq/_layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { meta } from "@sveltech/routify";
+  meta.title = "FAQ";
+</script>
+
+<!-- routify:options index=100 -->
+
+<div class="c-content"><div class="c-container-horizontal c-container-horizontal--small">
+  <h1>FAQ</h1>
+  <slot />
+</div></div>
