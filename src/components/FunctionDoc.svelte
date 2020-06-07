ae9ab30485358b9e54702efa2c3ea7e7d106f040
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-06-07 18:00:53 +0200

    component update

--- src/components/FunctionDoc.svelte
+++ src/components/FunctionDoc.svelte
@@ -2,16 +2,18 @@
   import Prism from "svelte-prism";
   import { getContext } from "svelte";
   export let name = "";
+  export let tags = "";
   export let params = [];
+  export let types = [];
   export let type = "function";
 
   $: isFunction = type === "function";
-  $: paramNames = params.map(param => param.name);
+  $: paramNames = params.map((param) => param.name);
 
   $: hash = name
     .replace(/\$/, "")
     .split(/(?=[A-Z])/)
-    .map(x => x.toLowerCase())
+    .map((x) => x.toLowerCase())
     .join("-");
 
   function addHashElement(node) {
@@ -26,6 +28,12 @@
   h2 code {
     font-weight: 700;
   }
+  .tag {
+    font-size: 12px;
+    background: #d7f7ff;
+    padding: 6px 12px;
+    margin-right: 6px;
+  }
 </style>
 
 <div class="c-function-doc">
@@ -36,6 +44,11 @@
         {name}
         {#if isFunction}({paramNames.join(', ')}){/if}
       </code>
+      {#if tags}
+        {#each tags.split(',') as tag}
+          <span class="tag">{tag}</span>
+        {/each}
+      {/if}
     </h3>
   </div>
   <div class="c-function-doc__content">
@@ -43,22 +56,44 @@
       <ul>
         {#each params as param}
           <li>
-            {param.name}
-            <ul>
-              <li>
-                type:
-                <code>{`{${param.type}}`}</code>
+            <strong>{param.name}</strong>
+            <div>
+              type:
+              <code>{`{${param.type}}`}</code>
+              {#if param.default}
                 <span class="df">
                   (default value:
                   <code>{param.default}</code>
                   )
                 </span>
-              </li>
-              <li>{param.description}</li>
-            </ul>
+              {/if}
+            </div>
+            <div>{param.description}</div>
+
           </li>
         {/each}
       </ul>
+      {#if types.length}
+        <hr style="margin-top: 0; padding-top: 0" />
+        <div>
+          {#each types as type}
+            <div>
+              <div class="object-type">
+                <code class="object-type__title">{type.name}</code>
+                <ul>
+                  {#each Object.entries(type.properties) as [name, prop]}
+                    <li>
+                      <span>{name}</span>
+                      <code>{prop.type}</code>
+                      <span>{prop.description}</span>
+                    </li>
+                  {/each}
+                </ul>
+              </div>
+            </div>
+          {/each}
+        </div>
+      {/if}
     </div>
     <div class="c-content">
       <slot />
