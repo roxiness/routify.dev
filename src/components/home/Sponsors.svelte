4cdb80b482ff9b8332cd24bab57ee4b963e7fea1
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-06-11 11:00:11 +0200

    add Netlify logo

---
+++ src/components/home/Sponsors.svelte
@@ -0,0 +1,10 @@
+<style>
+  div {
+    text-align: center;
+  }
+</style>
+
+<div class="c-content">
+  <h3>Sponsors</h3>
+  <a href="https://www.netlify.com/"><img src="/sponsors/Netlify_logo.svg" alt=""></a>
+</div>
