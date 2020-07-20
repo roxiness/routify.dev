586ca600fd6da8e9658cf437d6fb7f89fa4c9ad8
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-07-20 15:43:03 +0200

    add more examples

---
+++ src/pages/examples/example-apps.svelte
@@ -0,0 +1,16 @@
+<div class="c-content">
+  <h1 class="c-h1">Example app</h1>
+  <p>The example app from the starter template can be seen here.</p>
+  <p>
+    <a href="https://example.routify.dev/example">
+      Example folder from starter template
+    </a>
+  </p>
+  <p>
+    <a href="https://blog-example.routify.dev">Blog example</a>
+  </p>
+  <p>
+    <a href="https://auth-example.routify.dev">Auth example</a>
+  </p>
+
+</div>
