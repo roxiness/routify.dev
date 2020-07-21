058c9ab6b08d6cdb01f12f762bded7d8b849ee05
Bob Fanger <b.fanger@wearetriple.com>
2020-07-21 16:25:25 +0200

    fix: Broken image url for netlify logo

--- src/components/home/Sponsors.svelte
+++ src/components/home/Sponsors.svelte
@@ -6,5 +6,5 @@
 
 <div class="c-content">
   <h3>Sponsors</h3>
-  <a href="https://www.netlify.com/"><img src="/sponsors/Netlify_logo.svg" alt="Netlify logo"></a>
+  <a href="https://www.netlify.com/"><img src="/images/sponsors/Netlify_logo.svg" alt="Netlify logo"></a>
 </div>
