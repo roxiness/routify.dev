6602034f7723c0f57683e6b89b4b5b11d8d57dec
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-08-22 11:23:08 +0200

    move apache config to example

--- src/pages/examples/apache-config.svelte
+++ src/pages/examples/apache-config.svelte
@@ -4,21 +4,11 @@
   import Prism from "svelte-prism";
 </script>
 
+<!-- routify:options index=200  -->
+
 <div class="c-content">
-    <h1>Deployments</h1>
 
-    <h2>Now</h2>
-    <p><em>Instructions to be added.</em></p>
-
-    <h2>Netlify</h2>
-    <p><em>Instructions to be added.</em></p>
-
-    <h2>Apache server</h2>
-    <p>Deploy the contents of the `dist` folder. We've had success with the following `.htaccess` configuration:</p>
-    
-    <script>
-      import Prism from "svelte-prism";
-    </script>
+    <h2>Deploying on Apache server</h2>
 
     <Prism>{`
       RewriteEngine On
