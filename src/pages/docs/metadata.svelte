5227ccf56a3d1259921b97712c71158f793265da
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-08-26 12:12:04 +0200

    metadata and metatags update

--- src/pages/docs/metadata.svelte
+++ src/pages/docs/metadata.svelte
@@ -1,24 +1,83 @@
 <script>
-    import Prism from "svelte-prism";
-    import { meta } from "@sveltech/routify";
-    meta.title = "Metadata";
+  import Prism from "svelte-prism";
+  import { meta } from "@sveltech/routify";
+  meta.title = "Metadata";
 </script>
 
 <!-- routify:options index=40 -->
 
-<h1 class="c-h1">Metadata</h1>
-<div class="c-content">
+<div class="c-content c-container-vertical">
+  <h1>Metadata</h1>
 
-    <p>Metadata allows custom data to be added to a route.</p>
-    <p>To add metadata, insert a comment with <code>routify:options</code></p>
+  <p>
+    Metadata refers to Routify's internal data and can be used to add custom
+    data to any route. This data can then be accessed without loading the
+    component.
+  </p>
+  <p>
+    To add metadata, insert a comment with
+    <code>routify:options</code>
+  </p>
 
-    <Prism language="html">{`<!-- routify:opt`}{`ions index=1 -->`}    </Prism>
+  <Prism language="html">{`<!-- routify:opt`}{`ions index=1 -->`}</Prism>
 
-    <p>The metadata can then be accessed through <code>$route.meta</code></p>
-    <Prism>
-        {`
-            import { route } from @sveltech/routify
-            const { meta } = $route
+  <p>
+    The metadata can then be accessed through
+    <code>$route</code>
+    ,
+    <code>$layout</code>
+    and
+    <code>$page</code>
+  </p>
+  <Prism language="javascript">
+    {`
+            import { route, layout, page } from @sveltech/routify
+
+            $route.meta //meta from the current page
+            $page.meta //meta from the current page
+            $layout.meta //meta from the closest layout
         `}
-    </Prism>
+  </Prism>
+</div>
+
+<div class="c-content c-container-vertical">
+  <h3>Example</h3>
+  <p>
+    Using a custom
+    <code>published</code>
+    prop to filter nodes and a custom
+    <code>tags</code>
+    prop to display tags.
+  </p>
+
+  <Prism language="html">
+    {`
+      <!-- _outify:option_ tags=['recipes', 'cupcakes'] -->
+      <!-- _outify:option_ published=true -->
+      `}
+  </Prism>
+
+  <Prism language="svelte">
+    {`
+          <_crip_>
+            import { layout } from @sveltech/routify
+          </_crip_>
+
+          {#each $layout.parent.children.filter(c => c.published) as node}
+             <div class="card">
+               <h3>{node.title}</h3>
+               <h5>{node.meta['tags'].join(', ')}</h5>
+               <a href={$url(node.path)}>Goto</a>
+             </div>
+          {/each}
+        `}
+  </Prism>
+</div>
+
+<div class="c-content">
+  <h3>Realworld example</h3>
+  <a
+    href="https://github.com/roxiness/routify-site-2020/blob/master/src/pages/blog/index.svelte">
+    Routify blog index
+  </a>
 </div>
---
+++ src/pages/docs/metatags.svelte
@@ -0,0 +1,97 @@
+<script>
+  import Prism from "svelte-prism";
+  import Note from "@/components/Note.svelte";
+</script>
+
+<!-- routify:options index=40 -->
+<!-- routify:options title="HTML Metatags" -->
+
+<div class="c-content">
+  <div class="c-container-vertical c-container-vertical--small">
+    <h1 class="c-h1">HTML Metatags and OpenGraph</h1>
+
+    <p>
+      HTML metatags and OG can be set with the
+      <code>metatags</code>
+      helper.
+    </p>
+
+    <p>
+
+    </p>
+
+    <Prism>
+      {`
+        import { metatags } from '@sveltech/routify'
+        metatags.description = 'Routes for Svelte'
+    `}
+    </Prism>
+    <p>Would render the following HTML:</p>
+    <Prism language="html">
+      {`
+        <meta name="description" content="Routes for Svelte">
+    <meta property="og:description" content="Routes for Svelte">
+    `}
+    </Prism>
+    <p>
+      Tags without a colon, like
+      <code>description</code>
+      will render a metatag and an OG metatag with the same name/property. Tags
+      with a colon like
+      <code>twitter:card</code>
+      will not generate OG data.
+    </p>
+  </div>
+  <div class="c-container-vertical c-container-vertical--small">
+    <h3 class="c-h3">Title</h3>
+    <p>
+      Setting
+      <code>metatags.title = "Routify"</code>
+      also generates
+      <code>{`<title>Routify</title>`}</code>
+    </p>
+    <Prism>
+      {`
+        import { metatags } from '@sveltech/routify'
+        metatags.title = 'Routify'
+    `}
+    </Prism>
+    <p>Would render the following HTML:</p>
+    <Prism language="html">
+      {`
+      <title>Routify</title>
+    <meta name="title" content="Routes for Svelte">
+    <meta property="og:title" content="Routes for Svelte">
+    `}
+    </Prism>
+  </div>
+  <div class="c-container-vertical c-container-vertical--small">
+    <h3 class="c-h3">Non-OpenGraph Metadata</h3>
+    <p>
+      You can set any kind of tag you want. For example, a <code>twitter:card</code> metatag can be created by doing the following
+    </p>
+    <Prism>
+      {`
+        import { metatags } from '@sveltech/routify'
+        metatags['twitter:card'] = 'summary_large_image'
+    `}
+    </Prism>
+    <p>Would render the following HTML:</p>
+    <Prism language="html">
+      {`
+        <meta name="twitter:card" content="summary_large_image">
+    `}
+    </Prism>
+  </div>
+  <div class="c-container-vertical c-container-vertical--small">
+    <h3 class="c-h3">Templates</h3>
+    <p>
+      Templates can be set with
+      <code>metatags.template(property, callback)</code> ie.
+      <code>metatags.template("title", value => `My Site - ${`{value}`}`)</code>.
+      If multiple layouts with templates are present, the layout nearest the root takes precedence.
+    </p>
+  </div>
+</div>
+
+<!-- TODO this should probably be in docs instead -->
