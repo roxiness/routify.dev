fb3450a3bb5903e2e25a5dd14080eb7c54a7d692
Josef-Aidt <josef.aidt@us.ibm.com>
2020-09-08 12:44:34 -0500

    add css animation to accordian caret

--- src/components/Accordion.svelte
+++ src/components/Accordion.svelte
@@ -10,13 +10,25 @@
 
 <button class="c-navigation-mobile__section-header" on:click="{handleClick}">
     <span>{title}</span>
-    {#if navOpen}
-        <Icon name="chevron-up" />
-    {:else}
+    <div data-nav-open={navOpen}>
         <Icon name="chevron-down" />
-    {/if}
+    </div>
 </button>
 
 {#if navOpen}
 <slot></slot>
 {/if}
+
+<style>
+    :global(div[data-nav-open] svg) {
+        display: block;
+        transition: transform 100ms ease;
+    }
+    :global(div[data-nav-open="false"] svg) {
+        transform: rotate3d(0, 0, 0, 0deg);
+    }
+
+    :global(div[data-nav-open="true"] svg) {
+        transform: rotate3d(0, 0, -1, -180deg);
+    }
+</style>
