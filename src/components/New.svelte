72ee80ece39280d1548ed927af96cb5f96d851ac
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-10-31 18:06:31 +0100

    add "new" label. Wolfr do we have css for this?

---
+++ src/components/New.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { formatRelative, formatDistance } from "date-fns";
+  export let date;
+  // three months
+  const maxAge = 1000 * 60 * 60 * 24 * 30 * 3;
+  $: isNew = Date.now() - Date.parse(date) < maxAge;
+  $: title = date && formatRelative(new Date(date), new Date())
+</script>
+
+<style>
+  span {
+    position: absolute;
+    background: #87a1e3;
+    color: white;
+    font-size: 10px;
+    padding: 0 4px;
+    border-radius: 4px;
+  }
+</style>
+
+{#if isNew}<span {title}>new</span>{/if}
