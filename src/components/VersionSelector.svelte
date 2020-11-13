fd9d8f29e7ca7f908602afd3094d60dd134aa7ba
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-11-13 18:54:55 +0100

    version selector

---
+++ src/components/VersionSelector.svelte
@@ -0,0 +1,23 @@
+<script>
+  let initial = 'http://v1.routify.dev'
+  let selected = initial
+
+  $: if(initial !== selected) window.location.href = selected
+</script>
+
+<style>
+  select {
+    margin:0  20px;
+    padding: 8px;
+    height: fit-content;
+    color: #aaa;
+    font-weight: bold;
+    font-size: 1.2em;
+    border: none;
+  }
+</style>
+
+<select bind:value={selected}>
+  <option value="http://v1.routify.dev">1.x</option>
+  <option value="http://routify.dev">2.x</option>
+</select>
