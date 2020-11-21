95ffee2ac2fc1824de610a1a7dbe1ed87d5c8de9
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-11-21 22:07:49 +0100

    searchify

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,38 +1,39 @@
 <script>
-    import NavItems from './nav/NavItems.svelte';
-    import NavItemsMobile from './nav/NavItemsMobile.svelte';
-    import VersionSelector from './VersionSelector.svelte'
+  import NavItems from "./nav/NavItems.svelte";
+  import NavItemsMobile from "./nav/NavItemsMobile.svelte";
+  import VersionSelector from "./VersionSelector.svelte";
+  import SearchBox from "./SearchBox.svelte";
 
-    let showMobileNav = false
-    let bodyElement = document.querySelector('body');
-    let htmlElement = document.querySelector('html');
+  let showMobileNav = false;
+  let bodyElement = document.querySelector("body");
+  let htmlElement = document.querySelector("html");
 
-    function handleToggleClick(e) {
-      showMobileNav = !showMobileNav
-    }
-
+  function handleToggleClick(e) {
+    showMobileNav = !showMobileNav;
+  }
 </script>
 
 <svelte:head>
-   {#if showMobileNav}
-      <style>
-         html {
-            overflow: hidden;
-         }
-      </style>
-   {/if}
+  {#if showMobileNav}
+    <style>
+      html {
+        overflow: hidden;
+      }
+    </style>
+  {/if}
 </svelte:head>
 
 <header class="c-header">
   <div class="c-container-horizontal">
-
     <div style="display: flex; align-items: center;">
       <a href="/" class="c-logo">
-        <img src="/images/logo.svg" alt="Routify">
+        <img src="/images/logo.svg" alt="Routify" />
       </a>
       <VersionSelector />
     </div>
-
+    <div>
+      <SearchBox />
+    </div>
 
     <nav class="c-navigation">
       <NavItems />
@@ -42,19 +43,38 @@
     <button
       class="c-navigation-toggle"
       class:c-navigation-toggle--active={showMobileNav}
-      on:click|preventDefault={handleToggleClick}
-    >
+      on:click|preventDefault={handleToggleClick}>
       <svg width="40" height="40" viewBox="0 0 40 40">
-        <rect fill="#000" width="18" height="2" x="11" y="19" class="c-navigation-toggle__line1"></rect>
-        <rect fill="#000" width="18" height="2" x="11" y="19" class="c-navigation-toggle__line2"></rect>
-        <rect fill="#000" width="18" height="2" x="11" y="19" class="c-navigation-toggle__line3"></rect>
+        <rect
+          fill="#000"
+          width="18"
+          height="2"
+          x="11"
+          y="19"
+          class="c-navigation-toggle__line1" />
+        <rect
+          fill="#000"
+          width="18"
+          height="2"
+          x="11"
+          y="19"
+          class="c-navigation-toggle__line2" />
+        <rect
+          fill="#000"
+          width="18"
+          height="2"
+          x="11"
+          y="19"
+          class="c-navigation-toggle__line3" />
       </svg>
       <span class="u-sr-only">Toggle navigation</span>
     </button>
   </div>
 </header>
 
-<div class="c-navigation-mobile-holder" class:c-navigation-mobile-holder--active={showMobileNav}>
+<div
+  class="c-navigation-mobile-holder"
+  class:c-navigation-mobile-holder--active={showMobileNav}>
   <nav>
     <NavItemsMobile bind:showMobileNav />
   </nav>
---
+++ src/components/SearchBox.svelte
@@ -0,0 +1,106 @@
+<script>
+  import { client } from "searchify/runtime";
+  import Icon from "./Icon.svelte";
+  let query = "";
+  let index = null;
+  let results = [];
+  if (routify.inBrowser) client.init().then((res) => (index = res));
+  const removeFocus = (el) => {document.activeElement.blur()}
+  $: index && (results = index.search(query));
+</script>
+
+<style>
+  :focus {
+    outline: none;
+    background: #fff6ff;
+  }
+  .results {
+    background: white;
+    position: absolute;
+    z-index: 1;
+    padding: 0;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.4);
+    border-radius: 2px;
+    display: none;
+  }
+  .search:focus-within .results {
+    display: block;
+  }
+  input {
+    border: 1px solid #aaa;
+    border-radius: 8px;
+    font-size: 1.2em;
+    padding: 8px;
+    color: #555;
+  }
+  input:focus {
+    border-color: #333;
+    background: inherit;
+  }
+  a {
+    text-decoration: none;
+    color: #1d2c48;
+    padding: 1.2rem 32px;
+    display: block;
+  }
+  li {
+    max-width: 480px;
+    border-bottom: #eee 1px solid;
+  }
+  li:first-child a {
+    padding-top: calc(1.2rem + 16px);
+  }
+  a:hover {
+    background: #fff6ff;
+  }
+  small {
+    padding-left: 8px;
+    color: #aaa;
+  }
+  .title {
+    display: inline-block;
+    font-weight: bold;
+  }
+  .summary {
+    color: #555;
+    font-size: 0.9em;
+  }
+  .more {
+    user-select: none;
+    padding: 16px 32px;
+  }
+  * :global(.o-svg-icon) {
+    display: inline;
+    position: relative;
+    top: 4px;
+    right: 36px;
+  }
+</style>
+
+<div class="search">
+  <div>
+    <input type="text" bind:value={query} />
+    <Icon name="search" />
+  </div>
+  {#if query}
+    <div class="results" tabindex="0">
+      <ul>
+        {#each results.slice(0, 8) as result}
+          <li>
+            <a href="/{result.path}" on:click={removeFocus}>
+              <span
+                class="title">{result.title.replace(/ - Routify$/, '')}</span>
+              <small> {result.path.split('/').slice(0, -1).join(' / ')} </small>
+              <div class="summary">{result.text.slice(0, 100)}...</div>
+            </a>
+          </li>
+        {:else}
+          <li class="more">no results</li>
+        {/each}
+        {#if results.length > 8}
+          <li class="more">{results.length - 8} more results</li>
+        {/if}
+      </ul>
+    </div>
+  {/if}
+</div>
