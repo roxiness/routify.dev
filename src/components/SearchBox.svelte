5ff72a9cb40567a35974581b2f5561268aa7ed49
Wolfr <johan.ronsse@gmail.com>
2020-11-30 21:54:05 +0100

    Improve search styling
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- src/components/SearchBox.svelte
+++ src/components/SearchBox.svelte
@@ -9,75 +9,117 @@
 </script>
 
 <style>
-  :focus {
-    outline: none;
-    background: #fff6ff;
+
+  .search-input {
+    position: relative;
+    margin-right: 2.4rem;
   }
+
   .results {
-    background: white;
+    background: #FFF;
     position: absolute;
     z-index: 1;
     padding: 0;
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.4);
-    border-radius: 2px;
+    border-radius: .3rem;
     display: none;
+    max-height: 85vh;
+    overflow: auto;
   }
+
+  @media (min-width: 700px) {
+    .results {
+      min-width: 48rem;
+    }
+  }
+
   .search:focus-within .results {
     display: block;
   }
+
   input {
-    border: 1px solid #aaa;
-    border-radius: 8px;
-    font-size: 1.2em;
-    padding: 8px;
+    border: .1rem solid #AAA;
+    border-radius: .4rem;
+    font-size: 1.6rem;
+    padding: .8rem 3.6rem .8rem .8rem;
     color: #555;
   }
+
   input:focus {
-    border-color: #333;
-    background: inherit;
+    outline: none;
+    border-color: #BE46B7;
+    box-shadow: 0 0 0 .2rem #FFE4FF;
+    background: #FFF6FF;
   }
+
   a {
     text-decoration: none;
-    color: #1d2c48;
-    padding: 1.2rem 32px;
+    color: #1D2C48;
+    padding: 1.6rem 2.4rem;
     display: block;
   }
-  li {
-    max-width: 480px;
-    border-bottom: #eee 1px solid;
-  }
-  li:first-child a {
-    padding-top: calc(1.2rem + 16px);
-  }
+
   a:hover {
     background: #fff6ff;
   }
-  small {
-    padding-left: 8px;
-    color: #aaa;
+
+  li {
+    border-bottom: .1rem solid #EEE;
   }
+
+  li:first-child a {
+    padding-top: calc(1.2rem + 1.6rem);
+  }
+
+  small {
+    padding-left: .8rem;
+    font-size: 1.4rem;
+    color: #AAA;
+  }
+
   .title {
     display: inline-block;
-    font-weight: bold;
+    font-weight: 600;
+    font-size: 1.6rem;
   }
+
   .summary {
     color: #555;
-    font-size: 0.9em;
+    font-size: 1.3rem;
   }
+
   .more {
     user-select: none;
-    padding: 16px 32px;
+    padding: 1.6rem 3.2rem;
   }
-  * :global(.o-svg-icon) {
+
+  /* Icon */
+  .search-input :global(.o-svg-icon) {
     display: inline;
-    position: relative;
-    top: 4px;
-    right: 36px;
+    position: absolute;
+    opacity: 0.5;
+    right: 2.6rem;
+    top: .1rem;
+    width: 1.4rem;
+    height: 1.4rem;
   }
+
+  @media (min-width: 700px) {
+    .search-input :global(.o-svg-icon) {
+      width: 2rem;
+      height: 2rem;
+      right: 1.2rem;
+      top: .6rem;
+    }
+  }
+  .search-input:focus + :global(.o-svg-icon) {
+    opacity: 1;
+  }
+
 </style>
 
 <div class="search">
-  <div>
+  <div class="search-input">
     <input type="text" bind:value={query} />
     <Icon name="search" />
   </div>
@@ -94,7 +136,7 @@
             </a>
           </li>
         {:else}
-          <li class="more">no results</li>
+          <li class="more">No results</li>
         {/each}
         {#if results.length > 8}
           <li class="more">{results.length - 8} more results</li>
