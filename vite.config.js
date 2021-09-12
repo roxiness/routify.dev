9cc10db34ae4765e33847e0eac6669466042f2a2
GHOST <ghostdevbusiness@gmail.com>
2021-09-12 21:45:44 +0100

    fix: icons

--- vite.config.js
+++ vite.config.js
@@ -30,13 +30,13 @@ export default defineConfig({
                 display: 'standalone',
                 icons: [
                     {
-                        src: 'public/icons/pwa/manifest-icon-192.png',
+                        src: '/icons/pwa/manifest-icon-192.png',
                         sizes: '192x192',
                         type: 'image/png',
                         purpose: 'maskable any',
                     },
                     {
-                        src: 'public/icons/pwa/manifest-icon-512.png',
+                        src: '/icons/pwa/manifest-icon-512.png',
                         sizes: '512x512',
                         type: 'image/png',
                         purpose: 'maskable any',
