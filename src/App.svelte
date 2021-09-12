650ba360e0942725cdb2f421548ca3a5751aabb6
GHOST <ghostdevbusiness@gmail.com>
2021-09-12 19:22:28 +0100

    fix: remove old

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,6 @@
 <script>
-  import { setContext } from "svelte"
-  import { Router } from "@roxi/routify"
-  import { routes } from "../.routify/routes"
-  import { writable } from "svelte/store"
-
-  setContext("hashElements", new Map());
-
+    import { Router } from '@roxi/routify';
+    import { routes } from '../.routify/routes';
 </script>
 
-<!-- src/App.svelte -->
 <Router {routes} />
