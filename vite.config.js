909574a6f751b2c43aaaf8c0e8f3c877bb2df760
GHOST <ghostdevbusiness@gmail.com>
2021-09-12 21:43:23 +0100

    feat: pwa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-    plugins: [svelte()],
-
     server: {
         port: 5000,
     },
@@ -17,4 +16,33 @@ export default defineConfig({
             },
         ],
     },
+
+    plugins: [
+        svelte(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                background_color: '#ffffff',
+                theme_color: '#E938C2',
+                name: 'Routify',
+                short_name: 'Routify',
+                start_url: '.',
+                display: 'standalone',
+                icons: [
+                    {
+                        src: 'public/icons/pwa/manifest-icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                        purpose: 'maskable any',
+                    },
+                    {
+                        src: 'public/icons/pwa/manifest-icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable any',
+                    },
+                ],
+            },
+        }),
+    ],
 });
