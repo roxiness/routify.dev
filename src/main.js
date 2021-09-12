636ac410e3c10a350346532e4cd2e4188ff38aa0
GHOST <ghostdevbusiness@gmail.com>
2021-09-12 21:47:55 +0100

    feat: service worker

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import '../style/global.scss';
+import { registerSW } from 'virtual:pwa-register';
 import App from './App.svelte';
 
+// Update/register service worker
+registerSW()();
+
+// Create app
 const app = new App({
     target: document.body,
 });
