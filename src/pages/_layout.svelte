4e48e6836a98d1ee835820d6deecc818afd2e29b
GHOST <ghostdevbusiness@gmail.com>
2021-09-23 01:14:13 +0100

    feat: banner

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,1 +1,19 @@
+<banner>
+    Routify 3 is in <b>BETA</b>, not all features are here or documented
+</banner>
+
 <slot />
+
+<style>
+    banner {
+        width: 100%;
+        display: block;
+
+        padding: 12px;
+
+        text-align: center;
+
+        color: rgb(206, 47, 85);
+        background-color: #facffa;
+    }
+</style>
