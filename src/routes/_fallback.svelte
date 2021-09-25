45eb2b7dfb198fe20525dbb3f69ec9053bb2e197
GHOST <ghostdevbusiness@gmail.com>
2021-09-25 23:02:59 +0100

    feat: fallback

---
+++ src/routes/_fallback.svelte
@@ -0,0 +1,23 @@
+<div>
+    <h1>404</h1>
+
+    <p>
+        Unable to find the page you are looking for!
+        <a href="/">Go back</a>
+    </p>
+</div>
+
+<style lang="scss">
+    div {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        text-align: center;
+
+        h1 {
+            color: var(--theme);
+        }
+    }
+</style>
