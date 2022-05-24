956da1d56b79f0b8c298e35943f559ab3e2d6df8
GHOST <ghostdevbusiness@gmail.com>
2022-05-24 11:32:18 +0100

    feat: service worker

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // vite.config.js
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import preprocess from "svelte-preprocess";
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import { mdsvex } from "mdsvex";
 import { resolve } from "path";
@@ -22,6 +23,43 @@ export default defineConfig({
       ],
 
       extensions: [".svelte", ".md"],
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        background_color: "#ffffff",
+        theme_color: "#E938C2",
+        name: "Routify",
+        short_name: "Routify",
+        start_url: ".",
+        display: "standalone",
+        icons: [
+          {
+            "src": "public/icons/pwa/manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "public/icons/pwa/manifest-icon-192.maskable.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "public/icons/pwa/manifest-icon-512.maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "public/icons/pwa/manifest-icon-512.maskable.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+      },
     }),
   ],
 
